f7255b92f6099e61f885799aa46822cb5ba65082
田澤諒 <tazaryo0403@gmail.com>
2019-11-04 16:22:05 +0900

    get_word_info.pyを実行時に、引数の単語をDBに保存

--- db/db_client.py
+++ db/db_client.py
@@ -8,6 +8,12 @@ class DbClient(object):
     def __init__(self):
         self.words = Words()
 
+    def insert_word(self, tgt_word):
+        word = Words()
+        word.word = tgt_word
+        session.add(word)
+        session.commit()
+
     def get_words(self):
         words = session.query(Words).all()
         return words
